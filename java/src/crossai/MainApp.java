e8415fe082f01558c9f978cf5cb5301c2d30a597
Paweł Siurek <pawel.siurek77@gmail.com>
2026-01-21 17:12:41 +0100

    cleanup: little cleanup

--- java/src/crossai/MainApp.java
+++ java/src/crossai/MainApp.java
@@ -10,14 +10,10 @@ import javax.swing.UIManager;
 public class MainApp {
     
     public static void main(String[] args) {
-        // Set system look and feel
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
-            // Use default if system L&F fails
         }
-        
-        // Launch the modern UI on Event Dispatch Thread
         SwingUtilities.invokeLater(() -> new ModernMainView());
     }
 }
